ad07e239694d6615ec6adebe849d3e9747dac480
Armen Vardanyan <armenvardanyan95@gmail.com>
2023-10-12 12:16:17 +0400

    Try another

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,74 @@
-import { Routes } from '@angular/router';
+import { Component, inject, Input } from "@angular/core";
+import { CommonModule, NgFor } from "@angular/common";
+import {
+  RouterLink,
+  Routes,
+  RouterLinkActive,
+} from "@angular/router";
+import { Title } from "@angular/platform-browser";
 
-export const routes: Routes = [];
+@Component({
+  selector: "app-menu",
+  template: `
+    <ul>
+      <li *ngFor="let item of menuItems">
+        <a [routerLink]="['/', item.id]" routerLinkActive="active-page">{{
+          item.title
+        }}</a>
+      </li>
+    </ul>
+  `,
+  standalone: true,
+  imports: [NgFor, RouterLink, RouterLinkActive],
+})
+export class MenuComponent {
+  @Input() menuItems: { title: string; id: string }[] = [];
+}
+
+export abstract class BasePage {
+  abstract title: string;
+
+  get menuItems() {
+    return [
+      { title: `From ${this.title} to First`, id: "first" },
+      { title: `From ${this.title} to Second`, id: "second" },
+      { title: `From ${this.title} to Third`, id: "third" },
+    ];
+  }
+}
+
+@Component({
+  selector: "app-first",
+  template: `First Menu: <app-menu [menuItems]="menuItems" />`,
+  standalone: true,
+  imports: [MenuComponent],
+})
+export class FirstComponent extends BasePage {
+  title = inject(Title).getTitle();
+}
+
+@Component({
+  selector: "app-second",
+  template: `Second Menu: <app-menu [menuItems]="menuItems" />`,
+  standalone: true,
+  imports: [MenuComponent],
+})
+export class SecondComponent extends BasePage {
+  title = inject(Title).getTitle();
+}
+
+@Component({
+  selector: "app-third",
+  template: `Third Menu: <app-menu [menuItems]="menuItems" />`,
+  standalone: true,
+  imports: [MenuComponent],
+})
+export class ThirdComponent extends BasePage {
+  title = inject(Title).getTitle();
+}
+
+export const routes: Routes = [
+  { path: "first", component: FirstComponent, title: "First" },
+  { path: "second", component: SecondComponent, title: "Second" },
+  { path: "third", component: ThirdComponent, title: "Third" },
+];
